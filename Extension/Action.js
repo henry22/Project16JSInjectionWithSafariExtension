14e0739c09b6c50b917a0cf6e3a15b6e0fb9845f
henry22 <henry811010@gmail.com>
2015-06-24 20:56:21 +0800

    If user has written their javascript code in the extension, tapped Done, and it gets executed in Safari.

--- Extension/Action.js
+++ Extension/Action.js
@@ -3,11 +3,13 @@ var Action = function() {};
 Action.prototype = {
     
 run: function(parameters) {
-    
+    parameters.completionFunction({"URL": document.URL, "title": document.title })
 },
     
 finalize: function(parameters) {
-    
+    var customJavaScript = parameters["customJavaScript"];
+    //executes any code it finds
+    eval(customJavaScript);
 }
     
 };
--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -11,21 +11,37 @@ import MobileCoreServices
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    
+    var pageTitle = ""
+    var pageURL = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //make that call done()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "done")
         
         //let us control how it interacts with the parent app, and this will be an array of data the parent app is sending to our extension to use
-        if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             //pulls out the first attachment from the first input item
             if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
                 //ask the item provider to actually provide us with its item
                 itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options:
                     nil) { [unowned self] (dict, error) in
-                    //do stuff
-                }
+                        //get a dictionary of data that contains all the information
+                        let itemDictionary = dict as! NSDictionary
+                        // the data sent from JavaScript, and stored in a special key 
+                        let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+                        
+                        self.pageTitle = javaScriptValues["title"] as! String
+                        self.pageURL = javaScriptValues["URL"] as! String
+                        
+                        dispatch_async(dispatch_get_main_queue()) {
+                            //set the view controller's title property on the main queue
+                            self.title = self.pageTitle
+                        }
+                 }
             }
         }
     }
@@ -36,9 +52,15 @@ class ActionViewController: UIViewController {
     }
 
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequestReturningItems(self.extensionContext!.inputItems, completionHandler: nil)
+        //new NSExtensionItem object that will host our items
+        let item = NSExtensionItem()
+        //a dictionary containing the key "customJavaScript" and the value of the textView
+        //Put that dictionary into another dictionary
+        let webDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: ["customJavaScript": textView.text]]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        
+        extensionContext!.completeRequestReturningItems([item], completionHandler: nil)
     }
 
 }
