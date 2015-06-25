ca85a1c1d922008b00d67e246488513a86a015d9
henry22 <henry811010@gmail.com>
2015-06-25 20:50:21 +0800

    Fixing the keyboard.

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -22,6 +22,11 @@ class ActionViewController: UIViewController {
         //make that call done()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "done")
         
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        //four parameters: the object that should receive notifications, the method that should be called, the notification we want to receive, and the object we want to watch
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+        
         //let us control how it interacts with the parent app, and this will be an array of data the parent app is sending to our extension to use
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             //pulls out the first attachment from the first input item
@@ -44,6 +49,28 @@ class ActionViewController: UIViewController {
                  }
             }
         }
+    }
+    
+    func adjustForKeyboard(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        
+        //telling us the frame of the keyboard after it has finished animating
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        //need to convert the rectangle to our view's co-ordinates
+        let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+        
+        if notification.name == UIKeyboardWillHideNotification {
+            textView.contentInset = UIEdgeInsetsZero
+        } else {
+            //setting the inset of a text view
+            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        textView.scrollIndicatorInsets = textView.contentInset
+        
+        let selectedRange = textView.selectedRange
+        //make the text view scroll so that the text entry cursor is visible
+        textView.scrollRangeToVisible(selectedRange)
     }
 
     override func didReceiveMemoryWarning() {
